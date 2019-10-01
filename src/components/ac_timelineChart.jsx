4a54b026a90303caca7a4b399399fa91e7c7391b
malinsan <malin.thelin12@gmail.com>
2019-10-01 14:41:16 +0200

    Create timeline markers

--- src/components/ac_timelineChart.jsx
+++ src/components/ac_timelineChart.jsx
@@ -1,95 +1,107 @@
 import React from 'react'
 import Chart from "react-apexcharts"
+import { colors } from '../util/colors';
 
 export class AC_TimelineChart extends React.Component {
     constructor(props) {
         super(props);
     
         this.state = {
-          options: {
-            chart: {
-              id: "basic-bar"
+            options: {
+                chart: {
+                    id: "basic-bar",
+                    toolbar: { show: false }
+                },
+                xaxis: { categories: this.data.map(e => e[0]) }
             },
-            xaxis: {
-              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-            }
-          },
-          series: [
-            {
+            series: [{
               name: "series-1",
               data: [30, 40, 45, 50, 49, 60, 70, 91]
-            }
-          ],
-          timelineOptions: {
-            grid: {
+            }],
+            
+            timelineOptions: {
+                chart: { 
+                    toolbar: { show: false },
+                    events: {
+                        dataPointsMouseEnter: () => console.log('HALLÅ ELLER')
+                    },
+                    
+                },
+                tooltip: {
+                    shared: false,
+                    intersect: true
+                },
+                grid: {
+                    yaxis: {
+                        lines: { show: false }
+                    }
+                },
                 yaxis: {
-                    lines: {
-                        show: false
+                    axisTicks: { show: false },
+                    labels: { style: { color: 'rgba(0, 0, 0, 0)' } }
+                },
+                xaxis: {
+                    floating: true,
+                    axisTicks: { show: false },
+                    axisBorder: { show: false },
+                    labels: { show: false },
+                },
+                //annotations: { points: this.data.map(this.createPoints) }
+                markers: {
+                    size: 8,
+                    colors: colors.evOrange,
+                    strokeColors: '#fff',
+                    strokeWidth: 2,
+                    hover: {
+                        size: 14
                     }
                 }
-            },
-            yaxis: {
-              axisTicks: {
-                  show: false
-              },
-              labels: {
-                  style: {
-                      color: 'rgba(0, 0, 0, 0)'
-                  }
-              }
-            },
-            xaxis: {
-                floating: true,
-                axisTicks: {
-                  show: false
-                },
-                axisBorder: {
-                  show: false
-                },
-                labels: {
-                  show: false
-                },
-            },
-            annotations: {
-                points: [{
-                    x: 1991,
-                    y: 1,
-                    marker: {
-                        size: 8,
-                        fillColor: '#fff',
-                        strokeColor: 'red',
-                        radius: 2,
-                        cssClass: 'apexcharts-custom-class'
-                    },
-                    label: {
-                        borderColor: '#FF4560',
-                        offsetY: 0,
-                        style: {
-                            color: '#fff',
-                            background: '#FF4560',
-                        },
-                        text: 'Point Annotation',
-                    }
-                }]
             }
-          }
         }
-      }
+    }
+
+    data = [[1991, 1], [1992, 1], [1993, 1], [1994, 1], [1995, 1], [1996,1], [1997,1], [1998,1], [1999,1]]
+
+    createPoints(e) {
+        return {
+            x: e[0],
+            y: e[1],
+            marker: {
+                size: 8,
+                fillColor: '#fff',
+                strokeColor: 'red',
+                radius: 2,
+                cssClass: 'apexcharts-custom-class'
+            },
+            label: {
+                borderColor: '#FF4560',
+                offsetY: 0,
+                style: {
+                    color: '#fff',
+                    background: '#FF4560'
+                },
+                text: 'Hallå eller'
+            }
+        }
+    }
     
-      render() {
-        return ( 
-        <div>
+    render() {
+        const { options, series, timelineOptions } = this.state
+
+
+        return (<div>
             <Chart
-                options={this.state.timelineOptions}
-                series={[{ data: [[1991, 1], [1992, 1], [1993, 1], [1994, 1], [1995, 1], [1996,1], [1997,1], [1998,1], [1999,1]] }]}
-                height="50%"
+                options={timelineOptions}
+                series={[{ data: this.data }]}
+                height="100px"
             />
             <Chart
-                options={this.state.options}
-                series={this.state.series}
+                options={options}
+                series={series}
                 type="line"
                 width="100%"
+                height="500px"
             />
         </div>)
-      }
+    }
 }
---
+++ src/util/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+    evGreen: '#83AB48',
+    evDarkBlue: '#1A3050',
+    evLightBlue: '#51BEAA',
+    evDarkGreen: '#6C8A82',
+    evPurple: '#564566',
+    evPink: '#B55C80',
+    evRed: '#C53C31',
+    evOrange: '#EE7F4B'
+}
